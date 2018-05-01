3e808b4d1df08ae3f7aa2ac9ad9c2365cb53c357
Wiliam <mellamowiliam@gmail.com>
2018-05-01 14:05:29 +0200

    refactorized

--- app.js
+++ app.js
@@ -1,66 +1,95 @@
+process.env.MODULE_DEBUG = process.NODE_ENV !== 'production';
 
-process.env.MODULE_DEBUG = (process.NODE_ENV == 'production' ? false : true);
+const pmx = require('pmx');
+const pm2 = require('pm2');
+const pkg = require('./package.json');
+const requestp = require('request-promise');
+const debug = require('debug')(pkg.name);
+const Promise = require('bluebird');
 
-var pmx     = require('pmx');
-var pm2     = require('pm2');
-var async   = require('async');
-var pkg     = require('./package.json');
-var debug   = require('debug')(pkg.name);
+// promisified
+const pm2List = Promise.promisify(pm2.list);
+const pm2PullReload = Promise.promisify(pm2.pullAndReload);
 
-var conf    = pmx.initModule();
+// config
+const conf = pmx.initModule();
 
-var Probe = pmx.probe();
-
-var app_updated = Probe.counter({
-  name : 'App updated'
-});
-
-function autoPull(cb) {
-  pm2.list(function(err, procs) {
-
-    async.forEachLimit(procs, 1, function(proc, next) {
-      if (proc.pm2_env && proc.pm2_env.versioning) {
-        debug('pull And Reload %s', proc.name);
-        pm2.pullAndReload(proc.name, function(err, meta) {
-          if (meta) {
-            var rev = meta.rev;
-
-            app_updated.inc();
-
-            if (rev)
-              console.log('Successfully pulled [App name: %s] [Commit id: %s] [Repo: %s] [Branch: %s]',
-                          proc.name,
-                          rev.current_revision,
-                          meta.procs[0].pm2_env.versioning.repo_path,
-                          meta.procs[0].pm2_env.versioning.branch);
-            else {
-              // Backward compatibility
-              console.log('App %s succesfully pulled');
-            }
-          }
-          if (err)
-            debug('App %s already at latest version', proc.name);
-          return next();
-        });
-      }
-      else next();
-    }, cb);
-
-  });
+async function notify(proc) {
+    console.info(proc);
 }
 
-pm2.connect(function() {
-  console.log('pm2-auto-pull module connected to pm2');
+/**
+ * Notifies a pm2 process, waits for a response, updates it and reload.
+ *
+ * @param proc
+ */
+async function pullProc(proc) {
+    if(!proc.pm2_env || proc.pm2_env.versioning) {
+        return;
+    }
 
-  var running = false;
+    debug('Check, pull and reload: %s', proc.name);
 
-  setInterval(function() {
-    if (running == true) return false;
+    // Check
+    // TODO: Connect, notify and wait response
 
-    running = true;
-    autoPull(function() {
-      running = false;
-    });
-  }, conf.interval || 30000);
+    // Pull
+    try {
+        let meta = await pm2PullReload(proc.name);
 
+        if(meta) {
+            let rev = meta.rev;
+
+            if(rev)
+                console.log('Successfully pulled [App name: %s] [Commit id: %s] [Repo: %s] [Branch: %s]',
+                    proc.name,
+                    rev.current_revision,
+                    meta.procs[0].pm2_env.versioning.repo_path,
+                    meta.procs[0].pm2_env.versioning.branch
+                );
+
+            else {
+                // Backward compatibility
+                console.log('App "%s" succesfully pulled');
+            }
+        }
+
+    } catch(err) {
+        debug('App "%s" already at latest version (msg: %s)', proc.name, err.message);
+    }
+
+}
+
+/**
+ * Iterate over all pm2 procs and checks them one by one.
+ */
+async function check() {
+    let procs = await pm2List();
+
+    for(let i = 0; i < procs.length; i++) {
+        let proc = procs[i];
+        await pullProc(proc);
+    }
+}
+
+/**
+ * Start function, checks and calls itself again.
+ */
+async function start() {
+    try {
+        await check();
+
+    } catch(err) {
+        console.error("Auto pull check error: " + err.message);
+    }
+
+    setTimeout(start, Math.max(conf.interval, 1000));
+}
+
+/**
+ * Module bootstrap
+ */
+pm2.connect(() => {
+    console.log('pm2-auto-pull module connected to pm2');
+    start();
 });
